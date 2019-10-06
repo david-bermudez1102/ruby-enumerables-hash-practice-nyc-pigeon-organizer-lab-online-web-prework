4ebb5517fd0a66c329c740abbf44cedd8f67e505
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-10-06 23:19:32 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,39 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  attributes = Hash.new { |h, k| h[k] = [] } #hash with the atributes color, gender, lives
+  result = Hash.new { |h, k| h[k] = [] } #hash with each person with no organized attributes
+
+  final = {} #Our final hash
+
+    data.each do |item,sub_item|
+      sub_item.each do |key2,value2|
+        value2.each do |item2|
+          result[item2] << key2.to_s
+        end
+      end
+    end
+
+    data.each do |item,sub_item|
+      sub_item.each do |key2,value2|
+        attributes[item] << key2.to_s
+      end
+    end
+
+    result.each do |item,sub_item|
+      attributes.each do |attribute,value|
+        final[item] = {}
+      end
+    end
+
+    attributes.each do |attribute,value|
+      result.each do |item,sub_item|
+        sub_item.each_with_object([]) do |attribute_value, array|
+          if(value.include?(attribute_value))
+           array << attribute_value
+          end
+          final[item][attribute] = array
+       end
+      end
+    end
+
+final
 end
