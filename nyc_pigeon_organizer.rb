addea0d38c32b60ed9d35ee754c1f1c1ae7fcfc0
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-10-06 22:53:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  attributes = Hash.new { |h, k| h[k] = [] }
+  result = Hash.new { |h, k| h[k] = [] }
+  final = {}
+
+    data.each do |item,sub_item|
+
+      sub_item.each do |key2,value2|
+
+        value2.each do |item2|
+          result[item2] << key2.to_s
+          final[item2] = {:item = ""}
+        end
+      end
+    end
+
+    data.each do |item,sub_item|
+
+      sub_item.each do |key2,value2|
+          attributes[item] << key2.to_s
+      end
+    end
+
+    result.each do |item,sub_item|
+      final[item] = {}
+    end
+
+
+final
 end
+nyc_pigeon_organizer(pigeon_data)
